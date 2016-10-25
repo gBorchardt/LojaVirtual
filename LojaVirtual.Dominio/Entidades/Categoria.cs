a9ef525cc7a3edf33f117e1f2038cb8322f862cc
g.madeira <g.madeira@NP-330>
2016-10-25 16:54:22 -0200

    Entidades Produto e Categoria

---
+++ LojaVirtual.Dominio/Entidades/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Dominio.Entidades
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        public string Descricao { get; set; }
+        public string CodigoExterno { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int ProdutoID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int CategoriaID { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
