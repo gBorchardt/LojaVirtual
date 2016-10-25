2d7cbc60ddbf07c4a187b0c5f855de1922a17ded
g.madeira <g.madeira@NP-330>
2016-10-25 21:41:54 -0200

    Incluido Entity Framework + DBContext + Repositorio

---
+++ LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LojaVirtual.Dominio.Entidades;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LojaVirtual.Dominio.Repositorio
+{
+    public class EfDbContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Categoria>().ToTable("Categorias");
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+        public DbSet<Categoria> Categorias{ get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ LojaVirtual.Dominio/Repositorio/ProdutoRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using LojaVirtual.Dominio.Entidades;
+using System.Collections.Generic;
+
+namespace LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutoRepositorio
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Categoria> categorias
+        {
+            get { return _context.Categorias; }
+        }
+
+        public IEnumerable<Produto> produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
