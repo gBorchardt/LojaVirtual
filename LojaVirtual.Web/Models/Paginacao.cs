7a7ff4ad0d1b958b124326dbb9bf3439e75951dc
g.madeira <g.madeira@NP-330>
2016-10-26 20:42:07 -0200

    Class paginação + Paginação Helper

---
+++ LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int QtdProdutosTotal { get; set; }
+
+        public int QtdProdutosPorPagina { get; set; }
+
+        public int PaginaAtual { get; set; }
+
+        public int TotalPagina
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)QtdProdutosTotal / QtdProdutosPorPagina);
+            }
+        }
+    }
+}
