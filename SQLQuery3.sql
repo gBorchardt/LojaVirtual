ae527776da8ce22b54aec31b6c5ecffd20d0a222
g.madeira <g.madeira@NP-330>
2016-11-07 12:53:26 -0200

    sql

---
+++ SQLQuery3.sql
@@ -0,0 +1,12 @@
+﻿select * from produto
+
+update produto set nome = 'Produto03' where ProdutoID = 3
+
+select * from categoria
+
+update categoria set descricao = 'Categoria02' where CategoriaId = 2
+
+
+select * from produto
+go
+select * from categoria
